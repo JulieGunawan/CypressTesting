f646ccd7874f5ca6da522f17d27ec4a1b7060743
JulieGunawan <julie.gunawan@protonmail.com>
2025-02-22 22:40:19 -0700

    adding custom command

--- cypress/e2e/fundamental.cy.ts
+++ cypress/e2e/fundamental.cy.ts
@@ -17,10 +17,8 @@ describe('Fundamental tests', () => {
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
-  it.only('Only focus on single test', () => {
-    // cy.get('[data-test="accordion-item-6"]').should('exist')
-    cy.get('[data-test="accordion-item-6-title"]').contains('Fundamental 6) Focussing on a single test')
-   
-  })
+  // it.only('Only focus on single test', () => {
+  //   cy.get('[data-test="accordion-item-6-title"]').contains('Fundamental 6) Focussing on a single test')  
+  // })
 
 })
