15015f6629a944ec9e9e8f1a96394f74a1cda768
JulieGunawan <julie.gunawan@protonmail.com>
2025-02-23 19:58:00 -0700

    add multi-pages testing

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
-export default function NavItem({label,path}){
+export default function NavItem({label,path, dataTest}:{label: string, path: string, dataTest: string}){
     return (
-        <Link href={path}>
+        <Link href={path} data-test={dataTest}>
             {label}
         </Link>
     )
